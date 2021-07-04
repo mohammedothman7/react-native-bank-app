b4f71ea031a9e4b2e2ad177793305a21405d35f1
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2021-07-03 20:55:15 -0400

    Added Cards component to render cards on home screen

---
+++ app/components/Home/Cards.js
@@ -0,0 +1,78 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+const Cards = ({ item, onPress, backgroundColor, textColor }) => {
+  const { title, amount, lastFourDigits, cardColor } = item;
+  return (
+    <TouchableOpacity style={[styles.container]}>
+      <LinearGradient
+        style={[styles.background]}
+        colors={["#eaeaea", cardColor]}
+      >
+        <View style={styles.wrapper}>
+          <Image
+            style={styles.image}
+            source={require("../../../assets/images/visa-logo.png")}
+          />
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.amount}>$ {amount}</Text>
+          <Text style={styles.lastFourDigits}>** {lastFourDigits}</Text>
+        </View>
+      </LinearGradient>
+    </TouchableOpacity>
+  );
+};
+
+export default Cards;
+
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 20,
+    width: 150,
+    borderRadius: 30,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgba(0, 0, 0, 0.25)",
+        shadowOpacity: 4,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 4,
+      },
+    }),
+  },
+  background: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    borderRadius: 30,
+  },
+  wrapper: {
+    marginLeft: 16,
+  },
+  image: {
+    width: 40,
+    height: 20,
+    marginTop: 24,
+  },
+  title: {
+    marginTop: 20,
+  },
+  amount: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  lastFourDigits: {
+    marginTop: 26,
+    paddingBottom: 10,
+  },
+});
