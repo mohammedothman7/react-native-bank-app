1372de6b07d0bf72aa15b4433e5184a2c529e747
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2021-07-08 20:38:04 -0400

    Added pink and orange colors

--- assets/colors/colors.js
+++ assets/colors/colors.js
@@ -4,6 +4,8 @@ export const COLORS = {
   yellow: "#F2FE8D",
   mint: "#B2D0CE",
   lilac: "#AA9EB7",
+  pink: "#EA9CA1",
+  orange: "#E7B263",
   grayPrimary: "#252525",
   graySecondary: "#212121",
 };
